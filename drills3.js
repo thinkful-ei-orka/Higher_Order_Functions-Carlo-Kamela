06e0b19166daa94aebaec756ee8525be030ad9df
Carlo Paredes <c.nick.paredes@gmail.com>
2020-04-20 16:16:16 -0700

    Finished filter and bonus

--- drills3.js
+++ drills3.js
@@ -1,3 +1,5 @@
+// Repeat Function
+
 function repeat(fn, n) {
     for(let i = 0; i < n; i++) {
         fn();
@@ -14,3 +16,31 @@ function goodbye() {
 
 repeat(hello, 5);
 repeat(goodbye, 5);
+
+// Filter Function
+
+function filter(arr, fn) {
+    let newArray = [];
+
+    for(let i = 0; i < arr.length; i++) {
+        if(fn(arr[i])) {
+            newArray.push(arr[i]);
+        }
+    }
+    return newArray;
+}
+
+const myNames = ['Rich', 'Joe', 'Bhaumik', 'Ray'];
+
+const filteredNames = filter(myNames, function(name) {
+    return name[0] === 'R';
+}); 
+
+console.log(filteredNames);
+
+// Problem 2 Bonus
+console.log(filter(myNames, (n) => { return n[0] === 'R';}));
+
+// Functions as return values
+
+
